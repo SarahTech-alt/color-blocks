f0164f7193f32ae8f1de32a620a6680810767828
Sarah Fuoss <sfuoss@gmail.com>
2021-08-26 20:13:08 -0500

    cleaned up class names and index divs

--- client.js
+++ client.js
@@ -1,1 +1,36 @@
 console.log('ready');
+/**
+ * 
+ * 
+Add 4 buttons that read 'Red', 'Yellow', 'Green', and 'Blue'. When you click on those buttons, it should add a Color Block of that associated color to the DOM. Above the buttons, should be total counts for each colors relative to the number of times the button has been clicked, thus the total number of color blocks of that color.
+
+ * 
+ */
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    $("#red-button").on('click', addRedBlock)
+    $("#green-button").on('click', addGreenBlock)
+}
+
+function addRedBlock() {
+    console.log('Adding Red Block');
+    $(".content").append(`<div class = "red block"></div>`)
+
+}
+
+function addGreenBlock() {
+    console.log('Adding Green Block');
+    $(".content").append(`<div class = "green block"></div>`)
+}
+
+function addYellowBlock() {
+    console.log('Adding Yellow Block');
+    $(".content").append(`<div class = "yellow block"></div>`)
+}
+
+function addBlueBlock() {
+    console.log('Adding Blue Block');
+    $(".content").append(`<div class = "blue block"></div>`)
+}
